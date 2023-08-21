166539fcc0151f33b5fae2199156d5ced6fac5d0
Abhimanyu Kumar <72791295+mrpool720@users.noreply.github.com>
2023-08-21 21:39:26 +0530

    Initial commit

---
+++ src/main/java/com/demo/RestAPICrud/RestApiCrudApplication.java
@@ -0,0 +1,14 @@
+package com.demo.RestAPICrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiCrudApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/test/java/com/demo/RestAPICrud/RestApiCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.RestAPICrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
